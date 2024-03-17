2897f314bb96382b52042b6715bd2480b4cd4638
Nouf Faisal <nouf@alMacBook-Air-alkhas-bNouf.local>
2024-03-17 20:06:30 +0300

    inital first

---
+++ project/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  project
+//
+//  Created by Nouf Faisal  on 07/09/1445 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ project/projectApp.swift
@@ -0,0 +1,17 @@
+//
+//  projectApp.swift
+//  project
+//
+//  Created by Nouf Faisal  on 07/09/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct projectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
